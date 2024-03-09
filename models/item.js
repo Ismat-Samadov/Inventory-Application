03603728d6776928e28429f415b8bfd5c48f3522
Ismat-Samadov <ismetsemedov@gmail.com>
2024-03-09 19:33:57 +0400

    check

--- models/item.js
+++ models/item.js
@@ -4,13 +4,24 @@ const mongoose = require('mongoose');
 const itemSchema = new mongoose.Schema({
   name: {
     type: String,
-    unique: true // Ensures the name is unique
+    required: true,
+    unique: true // Ensure uniqueness of the name field
   },
   description: String,
   category: String,
   price: Number,
   numberInStock: Number,
   imageURL: String
+});
+
+// Middleware to append "_1" to the name if it already exists
+itemSchema.pre('save', async function(next) {
+  const self = this;
+  const existingItem = await self.constructor.findOne({ name: self.name });
+  if (existingItem) {
+    self.name += '_1';
+  }
+  next();
 });
 
 module.exports = mongoose.model('Item', itemSchema);
