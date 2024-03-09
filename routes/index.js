fae38b95bdbe98e54bd89ca79e461d5396cca290
Ismat-Samadov <ismetsemedov@gmail.com>
2024-03-09 19:48:15 +0400

    check

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+// routes/index.js
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const express = require('express');
+const router = express.Router();
+const Item = require('../models/item'); // Assuming you have a model for items
+
+// GET homepage
+router.get('/', async (req, res, next) => {
+    try {
+        // Query the database to fetch all items
+        const items = await Item.find();
+        // Render the homepage template and pass the items to it
+        res.render('index', { items });
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
